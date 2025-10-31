e81e234d546ddc5d6888b259d611ebce0841bad4
Matej Zelenak <maejzele@gmail.com>
2025-10-31 23:38:34 +0100

    add goal that can change with mouse position

--- a2/birds/main.py
+++ a2/birds/main.py
@@ -1,11 +1,16 @@
 import irsim
+from custom_behaviour import GOAL_POS
 
 # TRAIN
 env = irsim.make('mas_leader_follower.yaml')
 env.load_behavior('custom_behaviour')
 
 for _ in range(180_0):
+    mouse_pos = env.mouse.mouse_pos
+    if mouse_pos is not None:
+        GOAL_POS['x'], GOAL_POS['y'] = mouse_pos
+
     env.step()
-    env.render(0.02)
+    env.render(0.025)
 
 env.end(1)
