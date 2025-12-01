e8f07ac052d700b438c2af63a61d6dcba999574a
Matej Zelenak <maejzele@gmail.com>
2025-12-01 21:44:42 +0100

    update config

--- a3/config.py
+++ a3/config.py
@@ -3,15 +3,22 @@ GRID_Y = 10
 
 ROBOTS = [
     {"state": (0, 0, 0), "level": 10, "radius": 0.2},
-    {"state": (1, 1, 0), "level": 15, "radius": 0.2},
-    {"state": (2, 2, 0), "level": 20, "radius": 0.2},
-    {"state": (3, 3, 0), "level": 20, "radius": 0.2},
-    {"state": (4, 4, 0), "level": 20, "radius": 0.2},
+    {"state": (4, 0, 0), "level": 15, "radius": 0.2},
+    {"state": (9, 0, 0), "level": 15, "radius": 0.2},
+    # {"state": (2, 2, 0), "level": 20, "radius": 0.2},
+    # {"state": (3, 3, 0), "level": 20, "radius": 0.2},
+    # {"state": (4, 4, 0), "level": 20, "radius": 0.2},
 ]
 
 APPLES = [
-    [2, 0],
-    [0, 2],
-    [8, 2],
-    [2, 8]
+    [0, 4],
+    [1, 4],
+    [2, 4],
+    [3, 4],
+    [4, 4],
+    [5, 4],
+    [6, 4],
+    [7, 4],
+    [8, 4],
+    [9, 4]
 ]
