d7906e97c4cce8582cc1b7f9b59ae72289eea777
Matej Zelenak <maejzele@gmail.com>
2025-12-01 18:50:05 +0100

    add example config

---
+++ a3/config.py
@@ -0,0 +1,17 @@
+GRID_X = 10
+GRID_Y = 10
+
+ROBOTS = [
+    {"state": (0, 0, 0), "level": 10, "radius": 0.2},
+    {"state": (1, 1, 0), "level": 15, "radius": 0.2},
+    {"state": (2, 2, 0), "level": 20, "radius": 0.2},
+    {"state": (3, 3, 0), "level": 20, "radius": 0.2},
+    {"state": (4, 4, 0), "level": 20, "radius": 0.2},
+]
+
+APPLES = [
+    [2, 0],
+    [0, 2],
+    [8, 2],
+    [2, 8]
+]
