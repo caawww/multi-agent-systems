89b68b4a842cf246d4e41132b3fd97635ce85f93
Matej Zelenak <maejzele@gmail.com>
2025-12-01 21:41:23 +0100

    update main with the behaviour specified

--- a3/main.py
+++ a3/main.py
@@ -7,14 +7,32 @@ generate_yaml(
     world_size=(GRID_X, GRID_Y),
     robots=ROBOTS,
     apples=APPLES,
+    behaviour="train",
     output_file="grid.yaml"
 )
 
 env = irsim.make('grid.yaml')
 env.load_behavior('custom_behaviour')
 
-for _ in range(500):
+for _ in range(150 * 10_0):
     env.step()
-    env.render(0.01)
+    # env.render(0.01)
+
+env.end()
+
+generate_yaml(
+    world_size=(GRID_X, GRID_Y),
+    robots=ROBOTS,
+    apples=APPLES,
+    behaviour="test",
+    output_file="grid.yaml"
+)
+
+env = irsim.make('grid.yaml')
+env.load_behavior('custom_behaviour')
+
+for _ in range(120_0):
+    env.step()
+    env.render(0.5)
 
 env.end()
